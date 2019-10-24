19fc9479b9317a59bf5826a301a63cb7d51f3de2
Thaddeus <tdronski@gmail.com>
2019-10-24 14:30:25 -0700

    Lesson 04

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Thaddeus Dronski on 10/24/19.
+//  Copyright © 2019 Thaddeus Dronski. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closedPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Smack/Utilities/Contants.swift
@@ -0,0 +1,13 @@
+//
+//  Contants.swift
+//  Smack
+//
+//  Created by Thaddeus Dronski on 10/24/19.
+//  Copyright © 2019 Thaddeus Dronski. All rights reserved.
+//
+
+import Foundation
+//Segues
+
+let TO_LOGIN = "toLoginVC"
+
